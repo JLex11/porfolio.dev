6773787f5cded0687f22058b3a90ea28d242e326
JLex11 <chikyelfurioso2011@gmail.com>
2024-06-23 22:17:02 -0500

    adding i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: "https://alexander-porfolio-dev.vercel.app/",
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+  },
+});
---
+++ src/i18n/ui.ts
@@ -0,0 +1,137 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+
+export const showDefaultLang = false;
+
+export const ui = {
+  es: {
+    // nav
+    "nav.experience": "Experiencia",
+    "nav.projects": "Proyectos",
+    "nav.about": "Acerca de",
+    "nav.contact": "Contacto",
+    "nav.theme-toggle.label": "Elige el tema",
+    // seo
+    "seo.title":
+      "Porfolio de Alexander - Desarrollador y Programador Web con 4 años de experiencia",
+    "seo.description":
+      "Contrata a Alexander para crear tu aplicación web. Desarrollador Web. Especializado en crear aplicaciones únicas.",
+    // hero
+    "hero.hiring.badge": "Disponible para trabajar",
+    "hero.title": "Hey, soy Alexander",
+    "hero.text1": "+4 años de experiencia.",
+    "hero.text2": "Desarrollador de Software y Entusiasta de la Programación",
+    "hero.text3":
+      "de Medellin, Colombia. Especializado en el desarrollo de aplicaciones web.",
+    "hero.contact": "Contáctame",
+    "hero.linkedin": "LinkedIn",
+    // section
+    "section.experience.title": "Experiencia laboral",
+    "section.experience1.title": "Indexador PPC",
+    "section.experience1.date": "Julio 2022 - Junio 2024",
+    "section.experience1.company": "Talent.com",
+    "section.experience1.description":
+      "Optimicé la gestión de datos para ofertas de trabajo patrocinadas, logrando una mejora significativa en el rendimiento de los scripts de extracción. Implementé JavaScript avanzado y técnicas eficientes de PHP, reduciendo los tiempos de ejecución del código. Esto no solo promovió las mejores prácticas de desarrollo, resultando en un código más limpio y mantenible, sino que también disminuyó considerablemente los costos de computación en la nube.",
+    "section.projects.title": "Proyectos",
+    "section.projects1.title":
+      "One Anime - Un sitio para ver tus series de anime favoritas",
+    "section.projects1.description":
+      "Aplicación web para ver series de anime, implementando frontend desde cero con Next.js y backend creado con NodeJS, Express y otras tecnologias, Haciendo uso de web scraping para proveer la base datos de PostgreSQL hospedada en Supabase, manejando y almacenando imagenes en cloudflare R2",
+    "section.projects2.title": "AI Chat - Un chatbot que corre en local",
+    "section.projects2.description":
+      "Un chatbot que corre en local, utilizando tu propia computadora para mantener conversaciones privadas, sin utilizar la nube o APIs externas, utilizando un modelo de lenguaje basado en Llama 3, y utilizando el framework AstroJS",
+    "section.about.title": "Sobre mí",
+    "section.about.description.paragraph1":
+      "Me llamo John Alexander Calle pero mis amigos me llaman Alex. Empecé en la programación hace 5 años. Desde entonces he ido aprendiendo mas y mas, enfrentando obstaculos y superandome a si mismo.",
+    "section.about.description.paragraph2.part1":
+      "Algunos de mis puntos fuertes incluyen",
+    "section.about.description.paragraph2.part2":
+      "la recilencia para superar momentos dificiles y seguir creciendo tanto personal como profesionalmente.",
+    "section.about.description.paragraph2.part3":
+      "Uno de mis logros que destaco fue fomentar el",
+    "section.about.description.paragraph2.part4":
+      "aprendizaje y las buenas prácticas",
+    "section.about.description.paragraph2.part5":
+      "de desarrollo entre mis compañeros en mi último empleo.",
+    "section.about.description.paragraph3.part1":
+      "Me apasiona enfrentar desafíos complejos y brindar soluciones",
+    "section.about.description.paragraph3.part2": "creativas e innovadoras",
+    "section.about.description.paragraph3.part3":
+      ". Siempre busco mejorar mis habilidades, mantenerte actualizado con las últimas tendencias y mejores prácticas. Destaco por mi enfoque en el",
+    "section.about.description.paragraph3.part4": "trabajo en equipo",
+    "section.about.description.paragraph3.part5":
+      ", la calidad y el compromiso con los objetivos del proyecto.",
+    "section.about.description.paragraph3.part6": "Me desafío constantemente",
+    "section.about.description.paragraph3.part7":
+      ", pero también valoro el trabajo en equipo. Creo firmemente en la colaboración y en aprovechar las fortalezas colectivas para resolver problemas de manera eficiente.",
+    // footer
+    "footer.about.title": "Sobre mí",
+    "footer.contact.title": "Contacto",
+  },
+  en: {
+    // nav
+    "nav.experience": "Experience",
+    "nav.projects": "Projects",
+    "nav.about": "About",
+    "nav.contact": "Contact",
+    "nav.theme-toggle.label": "Choose a theme",
+    // seo
+    "seo.title":
+      "Alexander's Portfolio - Web Developer and Programmer with 4 years of experience",
+    "seo.description":
+      "Hire Alexander to create your web application. Web Developer. Specialized in creating unique applications.",
+    // hero
+    "hero.hiring.badge": "Available for work",
+    "hero.title": "Hey, I'm Alexander",
+    "hero.text1": "+4 years of experience.",
+    "hero.text2": "Software Developer and Programming Enthusiast",
+    "hero.text3":
+      "from Medellin, Colombia. Specialized in web application development.",
+    "hero.contact": "Contact me",
+    // section
+    "section.experience.title": "Work Experience",
+    "section.experience1.title": "PPC Indexer",
+    "section.experience1.date": "July 2022 - June 2024",
+    "section.experience1.description":
+      "Optimized data management for job-based offers, resulting in a significant improvement in the performance of the extraction scripts. Implemented advanced JavaScript and efficient PHP techniques, reducing the execution time of the code. This not only promoted best practices in development, resulting in a more clean and maintainable code, but also significantly reduced the costs of computation in the cloud.",
+    "section.projects.title": "Projects",
+    "section.projects1.title":
+      "One Anime - A web application for viewing anime series",
+    "section.projects1.description":
+      "Web application for viewing anime series, implemented from scratch with Next.js and backend created with NodeJS, Express and other technologies, Utilizing web scraping to provide the base data of PostgreSQL hosted in Supabase, managing and storing images in cloudflare R2",
+    "section.projects2.title":
+      "AI Chat - A local chatbot that runs on your own computer",
+    "section.projects2.description":
+      "A local chatbot that runs on your own computer to maintain private conversations, without using the cloud or external APIs, using a language model based on Llama 3, and using the AstroJS framework",
+    "section.about.title": "About me",
+    "section.about.description.paragraph1":
+      "My name is John Alexander Calle but my friends call me Alex. I started programming 5 years ago. Since then I have been learning more and more, facing obstacles and overcoming myself.",
+    "section.about.description.paragraph2.part1":
+      "Some of my strengths include",
+    "section.about.description.paragraph2.part2":
+      "relentless perseverance to overcome difficult moments and continue growing both personally and professionally.",
+    "section.about.description.paragraph2.part3":
+      "One of my achievements that I am proud of is fostering the",
+    "section.about.description.paragraph2.part4": "learning and best practices",
+    "section.about.description.paragraph2.part5":
+      "of development among my colleagues in my last job.",
+    "section.about.description.paragraph3.part1":
+      "I love facing complex challenges and providing solutions",
+    "section.about.description.paragraph3.part2": "creatively and innovatively",
+    "section.about.description.paragraph3.part3":
+      ". I am always looking to improve my skills, keep you updated with the latest trends and best practices. I stand out for my focus on",
+    "section.about.description.paragraph3.part4": "teamwork",
+    "section.about.description.paragraph3.part5":
+      ", The quality and commitment to the project's objectives.",
+    "section.about.description.paragraph3.part6": "I am constantly challenged",
+    "section.about.description.paragraph3.part7":
+      ", but I also value teamwork. I am a strong believer in collaboration and leveraging collective strengths to solve problems efficiently.",
+    // footer
+    "footer.about.title": "About me",
+    "footer.contact.title": "Contact me",
+  },
+} as const;
