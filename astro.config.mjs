56edaccce95ffbd4db3f78cdaf94a44b0a995e4f
Lexx <chikyelfurioso2011@gmail.com>
2024-07-05 16:38:37 -0500

    optimizing images and font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
+import compressor from "astro-compressor"
+import { defineConfig } from "astro/config"
 
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), compressor({brotli: true})],
   site: "https://alexander-porfolio-dev.vercel.app/",
   i18n: {
     defaultLocale: "es",
