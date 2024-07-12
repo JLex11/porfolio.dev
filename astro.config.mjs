0c357daefa11fad7bc9913981d7f8174f435f2a5
Lexx <chikyelfurioso2011@gmail.com>
2024-07-12 16:24:53 -0500

    format and lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import tailwind from "@astrojs/tailwind"
-import compressor from "astro-compressor"
-import { defineConfig } from "astro/config"
+import tailwind from '@astrojs/tailwind'
+import compressor from 'astro-compressor'
+import { defineConfig } from 'astro/config'
 
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), compressor({brotli: true})],
-  site: "https://alexander-porfolio-dev.vercel.app/",
-  i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
-  },
-});
+	integrations: [tailwind(), robotsTxt(), compressor({ brotli: true })],
+	site: 'https://alexander-porfolio-dev.vercel.app/',
+	i18n: {
+		defaultLocale: 'es',
+		locales: ['es', 'en'],
+	},
+})
