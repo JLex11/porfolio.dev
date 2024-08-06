de9153b955b812687b70a1a1b851d49d7aabaec8
Lexx <chikyelfurioso2011@gmail.com>
2024-08-06 13:16:33 -0500

    spell fixes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -17,8 +17,7 @@ export const ui = {
 		'nav.theme-toggle.label': 'Elige el tema',
 
 		// seo
-		'seo.title':
-			'Porfolio de Alexander - Desarrollador y Programador Web con 4 años de experiencia',
+		'seo.title': 'Porfolio de Alexander - Desarrollador y Programador Web con 4 años de experiencia',
 		'seo.description':
 			'Contrata a Alexander para crear tu aplicación web. Desarrollador Web. Especializado en crear aplicaciones únicas.',
 
@@ -27,14 +26,13 @@ export const ui = {
 		'hero.title': 'Hey, soy Alexander',
 		'hero.text1': '+4 años de experiencia.',
 		'hero.text2': 'Desarrollador de Software y Entusiasta de la Programación',
-		'hero.text3':
-			'de Medellin, Colombia. Especializado en el desarrollo de aplicaciones web.',
+		'hero.text3': 'de Medellin, Colombia. Especializado en el desarrollo de aplicaciones web.',
 		'hero.contact': 'Contáctame',
 		'hero.linkedin': 'LinkedIn',
 
 		// section
 		'section.experience.title': 'Experiencia laboral',
-		'section.experience1.title': 'Indexador PPC',
+		'section.experience1.title': 'Desarrollador web',
 		'section.experience1.date': 'Julio 2022 - Junio 2024',
 		'section.experience1.company': 'Talent.com',
 		'section.experience1.description':
@@ -46,7 +44,7 @@ export const ui = {
 		// one-anime
 		'section.projects1.title': 'One Anime',
 		'section.projects1.description':
-			'Aplicación web de anime creada con Next.js (frontend) y Node.js/Express (backend). Utiliza web scraping para alimentar una base de datos PostgreSQL en Supabase y gestion de imagenes con Cloudflare R2.',
+			'Aplicación web de anime creada con Next.js (frontend) y Node.js/Express (backend). Utiliza web scraping para alimentar una base de datos PostgreSQL en Supabase y gestion de imágenes con Cloudflare R2.',
 		// ai-chat
 		'section.projects2.title': 'AI Chat',
 		'section.projects2.description':
@@ -58,25 +56,19 @@ export const ui = {
 
 		'section.about.title': 'Sobre mí',
 		'section.about.description.paragraph1':
-			'Me llamo John Alexander Calle pero mis amigos me llaman Alex. Empecé en la programación hace 5 años. Desde entonces he ido aprendiendo mas y mas, enfrentando obstaculos y superandome a si mismo.',
-		'section.about.description.paragraph2.part1':
-			'Algunos de mis puntos fuertes incluyen',
+			'Me llamo John Alexander Calle pero mis amigos me llaman Alex. Empecé en la programación hace 5 años. Desde entonces he ido aprendiendo mas y mas, enfrentando obstáculos y superándome a si mismo.',
+		'section.about.description.paragraph2.part1': 'Algunos de mis puntos fuertes incluyen',
 		'section.about.description.paragraph2.part2':
-			'la recilencia para superar momentos dificiles y seguir creciendo tanto personal como profesionalmente.',
-		'section.about.description.paragraph2.part3':
-			'Uno de mis logros que destaco fue fomentar el',
-		'section.about.description.paragraph2.part4':
-			'aprendizaje y las buenas prácticas',
-		'section.about.description.paragraph2.part5':
-			'de desarrollo entre mis compañeros en mi último empleo.',
-		'section.about.description.paragraph3.part1':
-			'Me apasiona enfrentar desafíos complejos y brindar soluciones',
+			'la resiliencia para superar momentos difíciles y seguir creciendo tanto personal como profesionalmente.',
+		'section.about.description.paragraph2.part3': 'Uno de mis logros que destaco fue fomentar el',
+		'section.about.description.paragraph2.part4': 'aprendizaje y las buenas prácticas',
+		'section.about.description.paragraph2.part5': 'de desarrollo entre mis compañeros en mi último empleo.',
+		'section.about.description.paragraph3.part1': 'Me apasiona enfrentar desafíos complejos y brindar soluciones',
 		'section.about.description.paragraph3.part2': 'creativas e innovadoras',
 		'section.about.description.paragraph3.part3':
 			'. Siempre busco mejorar mis habilidades, mantenerte actualizado con las últimas tendencias y mejores prácticas. Destaco por mi enfoque en el',
 		'section.about.description.paragraph3.part4': 'trabajo en equipo',
-		'section.about.description.paragraph3.part5':
-			', la calidad y el compromiso con los objetivos del proyecto.',
+		'section.about.description.paragraph3.part5': ', la calidad y el compromiso con los objetivos del proyecto.',
 		'section.about.description.paragraph3.part6': 'Me desafío constantemente',
 		'section.about.description.paragraph3.part7':
 			', pero también valoro el trabajo en equipo. Creo firmemente en la colaboración y en aprovechar las fortalezas colectivas para resolver problemas de manera eficiente.',
@@ -94,8 +86,7 @@ export const ui = {
 		'nav.theme-toggle.label': 'Choose a theme',
 
 		// seo
-		'seo.title':
-			"Alexander's Portfolio - Web Developer and Programmer with 4 years of experience",
+		'seo.title': "Alexander's Portfolio - Web Developer and Programmer with 4 years of experience",
 		'seo.description':
 			'Hire Alexander to create your web application. Web Developer. Specialized in creating unique applications.',
 
@@ -104,14 +95,13 @@ export const ui = {
 		'hero.title': "Hey, I'm Alexander",
 		'hero.text1': '+4 years of experience.',
 		'hero.text2': 'Software Developer and Programming Enthusiast',
-		'hero.text3':
-			'from Medellin, Colombia. Specialized in web application development.',
+		'hero.text3': 'from Medellin, Colombia. Specialized in web application development.',
 		'hero.contact': 'Contact me',
 		'hero.linkedin': 'LinkedIn',
 
 		// section
 		'section.experience.title': 'Work Experience',
-		'section.experience1.title': 'PPC Indexer',
+		'section.experience1.title': 'Web developer',
 		'section.experience1.date': 'July 2022 - June 2024',
 		'section.experience1.company': 'Talent.com',
 		'section.experience1.description':
@@ -136,23 +126,18 @@ export const ui = {
 		'section.about.title': 'About me',
 		'section.about.description.paragraph1':
 			'My name is John Alexander Calle but my friends call me Alex. I started programming 5 years ago. Since then I have been learning more and more, facing obstacles and overcoming myself.',
-		'section.about.description.paragraph2.part1':
-			'Some of my strengths include',
+		'section.about.description.paragraph2.part1': 'Some of my strengths include',
 		'section.about.description.paragraph2.part2':
 			'relentless perseverance to overcome difficult moments and continue growing both personally and professionally.',
-		'section.about.description.paragraph2.part3':
-			'One of my achievements that I am proud of is fostering the',
+		'section.about.description.paragraph2.part3': 'One of my achievements that I am proud of is fostering the',
 		'section.about.description.paragraph2.part4': 'learning and best practices',
-		'section.about.description.paragraph2.part5':
-			'of development among my colleagues in my last job.',
-		'section.about.description.paragraph3.part1':
-			'I love facing complex challenges and providing solutions',
+		'section.about.description.paragraph2.part5': 'of development among my colleagues in my last job.',
+		'section.about.description.paragraph3.part1': 'I love facing complex challenges and providing solutions',
 		'section.about.description.paragraph3.part2': 'creatively and innovatively',
 		'section.about.description.paragraph3.part3':
 			'. I am always looking to improve my skills, keep you updated with the latest trends and best practices. I stand out for my focus on',
 		'section.about.description.paragraph3.part4': 'teamwork',
-		'section.about.description.paragraph3.part5':
-			", The quality and commitment to the project's objectives.",
+		'section.about.description.paragraph3.part5': ", The quality and commitment to the project's objectives.",
 		'section.about.description.paragraph3.part6': 'I am constantly challenged',
 		'section.about.description.paragraph3.part7':
 			', but I also value teamwork. I am a strong believer in collaboration and leveraging collective strengths to solve problems efficiently.',
