eee05877874732cf130567d7342a17c51df9c650
Lexx <chikyelfurioso2011@gmail.com>
2025-03-29 21:03:13 -0500

    migrating from tailwind v3 to v4, replacing a incompatible library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
-import tailwind from '@astrojs/tailwind'
 import compressor from 'astro-compressor'
 import { defineConfig } from 'astro/config'
 
 import robotsTxt from 'astro-robots-txt'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), robotsTxt(), compressor({ brotli: true })],
-	site: 'https://alexander-porfolio-dev.vercel.app/',
-	i18n: {
-		defaultLocale: 'es',
-		locales: ['es', 'en'],
-	},
+  integrations: [robotsTxt(), compressor({ brotli: true })],
+  site: 'https://alexander-porfolio-dev.vercel.app/',
+
+  i18n: {
+      defaultLocale: 'es',
+      locales: ['es', 'en'],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
